7f813ce84fe57b072f93d3405d5169107a9a6b14
sayko24 <sayko.lix24@gmail.com>
2026-05-21 18:21:11 +0200

    update puerta.ino

--- Yori/Nuevo_Robot_Fabrica/puerta/puerta.ino
+++ Yori/Nuevo_Robot_Fabrica/puerta/puerta.ino
@@ -4,26 +4,16 @@
 char ssid[] = "ROBOT_COMPETICION"; 
 char pass[] = "12345678"; 
 int status = WL_IDLE_STATUS;
-unsigned int localPort = 8081; // Puerto emparejado con el robot
+unsigned int localPort = 8081; 
 WiFiEspUDP Udp;
-
-// CONFIGURACIÓN DE IP FIJA PARA LA PUERTA
-IPAddress local_IP(192, 168, 4, 10); 
-IPAddress gateway(192, 168, 4, 1);   // La IP del robot
-IPAddress subnet(255, 255, 255, 0);
 
 void setup() {
   Serial.begin(115200);   
   Serial1.begin(115200);  
-
-  Serial.println(F("\n--- MEGA EXTERNA (PUERTA) INICIADA ---"));
   
   WiFi.init(&Serial1);
-  
-  // Forzamos a la placa a usar la IP fija antes de conectar
-  WiFi.config(local_IP); 
 
-  Serial.print(F("Conectando con IP Fija (192.168.4.10) a la red: "));
+  Serial.print(F("Conectando a la red del robot: "));
   Serial.println(ssid);
 
   while (status != WL_CONNECTED) {
@@ -46,14 +36,10 @@ void loop() {
       packetBuffer[len] = '\0';
       
       if (strncmp(packetBuffer, "ABRIR", 5) == 0) {
-        Serial.println(F("====================================="));
-        Serial.println(F("[DIRECTO] -> COMANDO RECIBIDO: ABRIR"));
-        Serial.println(F("====================================="));
+        Serial.println(F("\n[RECIBIDO] -> COMANDO: ABRIR"));
       } 
       else if (strncmp(packetBuffer, "CERRAR", 6) == 0) {
-        Serial.println(F("====================================="));
-        Serial.println(F("[DIRECTO] -> COMANDO RECIBIDO: CERRAR"));
-        Serial.println(F("====================================="));
+        Serial.println(F("\n[RECIBIDO] -> COMANDO: CERRAR"));
       }
     }
   }
